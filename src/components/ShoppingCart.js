f755a803552377b2805271ee35ca2c5020d153c9
NickySimps <nick.a.simpson@gmail.com>
2025-01-25 23:31:09 -0500

    stay length and guest count for accoms

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,10 +1,20 @@
-export const initializeSnipcart = () => {
-    const cartButton = document.createElement('button');
-    cartButton.classList.add('snipcart-checkout');
-    cartButton.textContent = 'Cart';
-    document.querySelector('header').appendChild(cartButton);
-  
-    ['item.adding', 'item.added', 'cart.ready'].forEach((event) => {
-      Snipcart.events.on(event, (data) => console.log(event, data));
-    });
+import { CONFIG } from './config.js';
+
+export const initializeCart = () => {
+    window.SnipcartSettings = {
+      publicApiKey: 'ZjFmOWZjNDUtM2VmMC00ZmExLTkzMTctMjExM2UzZDcwZjI3NjM4Njg4Nzc3OTMyNzM5MTQ3',
+      loadStrategy: 'on-user-interaction',
+      modalStyle: "side",
+      protocol: 'https',
+      domain: 'cdn.snipcart.com'
+    };
   };
+
+  const script = document.createElement('script');
+  script.src = "https://cdn.snipcart.com/themes/v3.0/default/snipcart.js";
+  document.body.appendChild(script);
+
+  const link = document.createElement('link');
+  link.rel = "stylesheet";
+  link.href = "https://cdn.snipcart.com/themes/v3.0/default/snipcart.css";
+  document.head.appendChild(link);
--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,14 @@ import { initializeUI } from './main.js';
 import { initializeAuth } from './auth.js';
 import { initializeServices } from './services.js';
 import { initializeForms } from './forms.js';
-import { initializeSnipcart } from './ShoppingCart.js';
+import { initializeNavigation } from './navigation.js';
+import { initializeCart } from './ShoppingCart.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initializeUI();
   initializeAuth();
   initializeServices();
   initializeForms();
-  initializeSnipcart
+  initializeNavigation();
+  initializeCart();
 });
