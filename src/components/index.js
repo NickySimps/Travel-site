2423447efff6ab10d07377572e6b889b8137ec6e
NickySimps <nick.a.simpson@gmail.com>
2025-03-12 20:28:17 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { CONFIG } from './config.js';
+import { CONFIG } from 'config.js';
 import { initializeUI } from './main.js';
 import { initializeAuth } from './auth.js';
 import { initializeServices } from './services.js';
